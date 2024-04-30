9c0b1eaf1c2cf6913df6ae98ba7c7a1cc5ad0e76
Eduardo <dev.zanntech@outlook.com>
2024-04-30 09:46:07 -0600

    it works

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:todo_planner/models/todo.dart';
 import 'package:todo_planner/theme/colors.dart';
 import 'package:todo_planner/widget/atoms/appbar.dart';
-import 'package:todo_planner/widget/atoms/searchBox.dart';
 import 'package:todo_planner/widget/atoms/todo_item.dart';
 
 class Home extends StatefulWidget {
@@ -20,6 +19,121 @@ class _HomeState extends State<Home> {
   void initState() {
     _foundToDo = todosList;
     super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBgColor,
+      appBar: const AppBarAtom(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 10, bottom: 10),
+                    child: const Text(
+                      'All TODOs',
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  if (_foundToDo.isEmpty)
+                    const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              Icon(Icons.error_outline,
+                                  size: 50, color: tdGrey),
+                              Text('No se encontró ningún resultado',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 20)),
+                            ],
+                          ),
+                        ]),
+                  for (ToDo todo in _foundToDo.reversed)
+                    ToDoItem(
+                      todo: todo,
+                      onToDoChanged: _handleToDoChange,
+                      onDeleteItem: _deleteToDoItem,
+                    )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: tdBlue,
+        onPressed: () {
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                height: 500,
+                color: Colors.white30,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20)),
+                            alignment: Alignment.center,
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 20),
+                            child: TextField(
+                              controller: _todoController,
+                              decoration: const InputDecoration(
+                                contentPadding: EdgeInsets.all(0),
+                                label: Text('Ingrese el nombre de su tarea'),
+                                border: InputBorder.none,
+                                hintStyle: TextStyle(color: tdGrey),
+                                hintText: 'Ingrese el nombre de su tarea',
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                              width: double.infinity,
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 40, vertical: 20),
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    _addToDoItem(_todoController.text);
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('Agregar'),
+                                ),
+                              ))
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        child: const Icon(
+          Icons.add,
+          size: 20,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 
   void _handleToDoChange(ToDo todo) {
@@ -61,47 +175,25 @@ class _HomeState extends State<Home> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: tdBgColor,
-      appBar: const AppBarAtom(),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          children: [
-            const searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 50, bottom: 20),
-                    child: const Text(
-                      'All TODOs',
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
-                    ),
-                  ),
-                  for (ToDo todo in _foundToDo.reversed)
-                    ToDoItem(
-                      todo: todo,
-                      onDeleteItem: () {},
-                      onToDoChanged: () {},
-                    )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: tdBlue,
-        onPressed: () {},
-        child: const Icon(
-          Icons.add,
-          size: 20,
-          color: Colors.white,
-        ),
+  Widget searchBox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        onChanged: (value) => _runFilter(value),
+        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+              Icons.search,
+              color: tdBlack,
+              size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+            border: InputBorder.none,
+            hintText: 'Search',
+            hintStyle: TextStyle(color: tdGrey)),
       ),
     );
   }
